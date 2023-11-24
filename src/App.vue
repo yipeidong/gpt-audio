de5fceba0ac6d7ace3d58b6d64d4c908aea69cc7
yipeidong <1315103511@qq.com>
2023-11-24 21:58:13 +0800

    #init : init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Ball from './components/Ball.vue';
+</script>
+
+<template>
+  <div class="app">
+    <Ball class="ball-com"></Ball>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background-color: #111;
+}
+
+.ball-com {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
---
+++ src/components/Ball.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="ball-com">
+    <div class="ball b1"></div>
+    <div class="ball b2"></div>
+    <div class="ball b3"></div>
+    <div class="ball b4"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.ball-com {
+  position: relative;
+  width: 100vw;
+  height: 100vw;
+  background-color: #000;
+  filter: contrast(80);
+}
+
+.ball {
+  width: 20vw;
+  height: 20vw;
+  background-color: #fff;
+  border-radius: 50%;
+  filter: blur(5vw);
+}
+
+.b1 {
+  position: relative;
+  left: 10px;
+  top: 10px;
+  animation: move 2s infinite;
+}
+
+@keyframes move {
+  from{
+    left: 10px;
+  }
+  to{
+    left: 100px;
+  }
+}
+
+</style>
